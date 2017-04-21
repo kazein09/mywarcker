2d51c498eb5a59dd7c016dda95c743f33e165db1
karen <karen@localhost.localdomain>
2017-04-21 18:20:22 +0200

    sending working code to wercker

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-// Package stringutil contains utility functions for working with strings.
-package stringutil
-1!@#
-// Reverse returns its argument string reversed rune-wise left to right.
 func Reverse(s string) string {
 	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
