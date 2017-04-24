1384e9fb946c13942bbc83bed2967303d6e445bc
Kalinux <kazein09@gmail.com>
2017-04-24 20:27:44 +0200

    removing errors

--- main.go
+++ main.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-    // Standard library packages
-    "fmt"
-    "strconv"
-    "log"
-    "net"
-    "net/http"
+import "fmt"
 
-    // Third party packages
-    "github.com/julienschmidt/httprouter"
-    "github.com/skratchdot/open-golang/open"
-)
-
-// GetLocalIP returns the non loopback local IP of the host
-func GetLocalIP() string {
-    addrs, err := net.InterfaceAddrs()
-    if err != nil {
-        return ""
-    }
-    for _, address := range addrs {
-        // check the address type and if it is not a loopback the display it
-        if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-            if ipnet.IP.To4() != nil {
-                return ipnet.IP.String()
-            }
-        }
-    }
-    return ""
+func main() {
+	fmt.Printf("Hello, world.\n")
 }
