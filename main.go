10d1e869edd04ee1bfda6201fa5063d01f29c0ad
Kalinux <kazein09@gmail.com>
2017-04-24 20:47:37 +0200

    new main go without error 2

--- main.go
+++ main.go
@@ -1,27 +1,36 @@
 package main
 
 import (
-    // Standard library packages
-    "fmt"
-    "strconv"
-    "log"
-    "net"
-    "net/http"
+	"errors"
+	"fmt"
+	"net"
 )
 
-ifaces, err := net.Interfaces()
-// handle err
-for _, i := range ifaces {
-    addrs, err := i.Addrs()
-    // handle err
-    for _, addr := range addrs {
-        var ip net.IP
-        switch v := addr.(type) {
-        case *net.IPNet:
-                ip = v.IP
-        case *net.IPAddr:
-                ip = v.IP
-        }
-        // process IP address
-    }
+func main() {
+
+	ip, err := externalIP()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(ip)
+
 }
+
+func externalIP() (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, iface := range ifaces {
+		if iface.Flags&net.FlagUp == 0 {
+			continue // interface down
+		}
+		if iface.Flags&net.FlagLoopback != 0 {
+			continue // loopback interface
+		}
+		addrs, err := iface.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
