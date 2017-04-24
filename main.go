2f683dff12f534b2e52bbb4605479fe31d35424c
Kalinux <kazein09@gmail.com>
2017-04-24 20:42:31 +0200

    new main go

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    // Standard library packages
+    "fmt"
+    "strconv"
+    "log"
+    "net"
+    "net/http"
 
-func main() {
-	fmt.Printf("Hello, world.\n")
+    // Third party packages
+    "github.com/julienschmidt/httprouter"
+    "github.com/skratchdot/open-golang/open"
+)
+
+ifaces, err := net.Interfaces()
+// handle err
+for _, i := range ifaces {
+    addrs, err := i.Addrs()
+    // handle err
+    for _, addr := range addrs {
+        var ip net.IP
+        switch v := addr.(type) {
+        case *net.IPNet:
+                ip = v.IP
+        case *net.IPAddr:
+                ip = v.IP
+        }
+        // process IP address
+    }
 }
